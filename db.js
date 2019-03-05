a02329ba1d33d1e8d51d181f22b4f839f1c5b953
Preston Chaplin <preston.chaplin@gmail.com>
2019-03-05 15:33:54 -0500

    updated for deploy3

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_products_categories',
-  {
-    logging: false
-  }
-);
+const conn = new Sequelize(process.env.DATABASE_URL);
+// const conn = new Sequelize(
+//   process.env.DATABASE_URL || 'postgres://localhost/acme_products_categories',
+//   {
+//     logging: false
+//   }
+// );
 const faker = require('faker');
 
 const Category = conn.define('category', {
